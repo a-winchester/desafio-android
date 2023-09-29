680131108962a2065d38e085c9ad898fbb581b4c
Ana Paula Freitas <anapaula.mafr@gmail.com>
2023-09-29 16:19:58 -0300

    🎨 Implementing NetworkModule logic to remove network info from MainActivity

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -6,44 +6,18 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.picpay.desafio.android.data.PicPayService
+import com.picpay.desafio.android.data.NetworkModule
 import com.picpay.desafio.android.domain.User
 import com.picpay.desafio.android.ui.UserListAdapter
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var adapter: UserListAdapter
-
-    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
-
-    private val gson: Gson by lazy { GsonBuilder().create() }
-
-    private val okHttp: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .build()
-    }
-
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(url)
-            .client(okHttp)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-    }
-
-    private val service: PicPayService by lazy {
-        retrofit.create(PicPayService::class.java)
-    }
 
     override fun onResume() {
         super.onResume()
@@ -56,7 +30,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         progressBar.visibility = View.VISIBLE
-        service.getUsers()
+        NetworkModule.picPayService.getUsers()
             .enqueue(object : Callback<List<User>> {
                 override fun onFailure(call: Call<List<User>>, t: Throwable) {
                     val message = getString(R.string.error)
--- app/src/main/java/com/picpay/desafio/android/data/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/data/PicPayService.kt
@@ -1,7 +1,12 @@
 package com.picpay.desafio.android.data
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.domain.User
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 
@@ -9,4 +14,27 @@ interface PicPayService {
 
     @GET("users")
     fun getUsers(): Call<List<User>>
+}
+
+object NetworkModule {
+    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+
+    private val gson: Gson by lazy { GsonBuilder().create() }
+
+    private val okHttp: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttp)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    val picPayService: PicPayService by lazy {
+        retrofit.create(PicPayService::class.java)
+    }
 }
